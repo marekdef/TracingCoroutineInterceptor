20d354cd57138b5107c3b9b15898f88d32079a56
Marek Defeciński <marekdef@gmail.com>
2026-03-29 11:41:49 +0200

    Switched to suspend fun main added cancellation

--- src/main/kotlin/pl/senordeveloper/coroutinesmastery/Main.kt
+++ src/main/kotlin/pl/senordeveloper/coroutinesmastery/Main.kt
@@ -3,35 +3,44 @@ package pl.senordeveloper.coroutinesmastery
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.cancellation.CancellationException
 import kotlin.time.Duration.Companion.milliseconds
 
-fun main() {
+suspend fun main() {
     val tracer = TracingCoroutineInterceptor()
 
-    runBlocking(tracer + CoroutineName("main")) {
-        launch(CoroutineName("child-1")) {
-            delay(100.milliseconds)
-            println("hello")
+    withContext(tracer + CoroutineName("main")) {
+        supervisorScope {
+            launch(CoroutineName("child-1")) {
+                delay(100.milliseconds)
+                println("hello")
+            }
+            launch(CoroutineName("child-2")) {
+                delay(200.milliseconds)
+            }
+            val async1 = async(CoroutineName("child-3")) {
+                delay(50.milliseconds)
+                "result1"
+            }
+            val async2 = async(CoroutineName("child-4")) {
+                delay(150.milliseconds)
+                cancel()
+                "result2"
+            }
+            try {
+                awaitAll(async1, async2)
+            } catch (_: CancellationException) {
+                println("child-4 was cancelled")
+            }
         }
-        launch(CoroutineName("child-2")) {
-            delay(200.milliseconds)
-        }
-        val async1 = async(CoroutineName("child-3")) {
-            delay(50.milliseconds)
-            "result1"
-        }
-        val async2 = async(CoroutineName("child-4")) {
-            delay(150.milliseconds)
-            "result2"
-        }
-        awaitAll(async1, async2)
     }
 
     tracer.events.forEach { println(it) }
 
     println(tracer.renderTree())
-
 }
